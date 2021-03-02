a7d6676afa9d1a47e39609dd80ed9a27abacdfcd
vikraamm <vikraamm95>
2021-03-02 15:29:57 -0500

    sdfds

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './tenor.gif';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+      <p className="sorry">Please Accept my Apology.</p>
+      <img src={logo} alt="loading..." />
+        <p className="pawmise">I paw-mise not to do it again and fortunate to meet someone like you.</p>
+     </div>
   );
 }
 
